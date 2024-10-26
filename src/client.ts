cab0ba805614effe4e2585ec51c98f28f6e4dde3
kk3939 <kyo51310@gmail.com>
2024-10-26 18:25:31 +0900

    change oxlint to biome

--- src/client.ts
+++ src/client.ts
@@ -2,15 +2,15 @@
 import { ApolloClient, InMemoryCache, gql } from "@apollo/client";
 
 const main = async () => {
-  const client = new ApolloClient({
-    uri: "http://localhost:4000/graphql",
-    cache: new InMemoryCache(),
-  });
-  try {
-    // データの取得
-client
-.query({
-  query: gql`
+	const client = new ApolloClient({
+		uri: "http://localhost:4000/graphql",
+		cache: new InMemoryCache(),
+	});
+	try {
+		// データの取得
+		client
+			.query({
+				query: gql`
     query GetBooks {
       getBooks {
         id
@@ -19,13 +19,13 @@ client
       }
     }
   `,
-})
-.then(result => console.log(result.data));
+			})
+			.then((result) => console.log(result.data));
 
-// データの作成
-client
-.mutate({
-  mutation: gql`
+		// データの作成
+		client
+			.mutate({
+				mutation: gql`
     mutation CreateBook($title: String!, $author: String!) {
       createBook(title: $title, author: $author) {
         id
@@ -34,17 +34,17 @@ client
       }
     }
   `,
-  variables: {
-    title: "GraphQL入門",
-    author: "田中 太郎",
-  },
-})
-.then(result => console.log(result.data));
+				variables: {
+					title: "GraphQL入門",
+					author: "田中 太郎",
+				},
+			})
+			.then((result) => console.log(result.data));
 
-// データの更新
-client
-.mutate({
-  mutation: gql`
+		// データの更新
+		client
+			.mutate({
+				mutation: gql`
     mutation UpdateBook($id: Int!, $title: String, $author: String) {
       updateBook(id: $id, title: $title, author: $author) {
         id
@@ -53,29 +53,29 @@ client
       }
     }
   `,
-  variables: {
-    id: 1,
-    title: "GraphQL基礎",
-  },
-})
-.then(result => console.log(result.data));
+				variables: {
+					id: 1,
+					title: "GraphQL基礎",
+				},
+			})
+			.then((result) => console.log(result.data));
 
-// データの削除
-client
-.mutate({
-  mutation: gql`
+		// データの削除
+		client
+			.mutate({
+				mutation: gql`
     mutation DeleteBook($id: Int!) {
       deleteBook(id: $id)
     }
   `,
-  variables: {
-    id: 1,
-  },
-})
-.then(result => console.log(result.data));
-  } catch (error) {
-    console.error(error);
-  }
-}
+				variables: {
+					id: 1,
+				},
+			})
+			.then((result) => console.log(result.data));
+	} catch (error) {
+		console.error(error);
+	}
+};
 
 main();
--- src/server.ts
+++ src/server.ts
@@ -1,26 +1,35 @@
 import "reflect-metadata";
-import { Resolver, Query, Mutation, Arg, Int, buildSchema, Field, ObjectType } from "type-graphql";
-import express from "express";
-import { ApolloServer } from "apollo-server-express";
+import { ApolloServer, type ServerRegistration } from "apollo-server-express";
+import express, { type Application } from "express";
+import {
+	Arg,
+	Field,
+	Int,
+	Mutation,
+	ObjectType,
+	Query,
+	Resolver,
+	buildSchema,
+} from "type-graphql";
 
 // TypeORM使った場合のEntityと型定義一緒にやる方法
 // https://github.com/MichalLytek/type-graphql/blob/v2.0.0-rc.2/examples/typeorm-basic-usage/entities/user.ts
 // 今回はTypeORM使わないので、ここでGraphQLの型定義を行う。
 @ObjectType()
 class Book {
-  @Field()
-  id: number;
-  @Field()
-  title: string;
-  @Field()
-  author: string;
+	@Field()
+	id: number;
+	@Field()
+	title: string;
+	@Field()
+	author: string;
 
-  // コンストラクタで必ず初期化して値が入ってくるようにする。
-  constructor(id: number, title: string, author: string) {
-    this.id = id;
-    this.title = title;
-    this.author = author;
-  }
+	// コンストラクタで必ず初期化して値が入ってくるようにする。
+	constructor(id: number, title: string, author: string) {
+		this.id = id;
+		this.title = title;
+		this.author = author;
+	}
 }
 // Resolverでクエリやミューテーションを定義する。
 // 今回はBook型のクエリやミューテーションを定義する。
@@ -28,60 +37,59 @@ class Book {
 // 今回は、getBooks, createBook, updateBook, deleteBookのクエリやミューテーションを定義する。
 @Resolver(Book)
 class BookResolver {
-  // 擬似的なDBとして、private books: Book[] = [];を定義する。
-  // DBとつなぎこむ場合は、ここでDBとの接続を行う。
-  private books: Book[] = [];
+	// 擬似的なDBとして、private books: Book[] = [];を定義する。
+	// DBとつなぎこむ場合は、ここでDBとの接続を行う。
+	private books: Book[] = [];
 
-  @Query(() => [Book])
-  getBooks(): Book[] {
-    return this.books;
-  }
+	@Query(() => [Book])
+	getBooks(): Book[] {
+		return this.books;
+	}
 
-  @Mutation(() => Book)
-  createBook(@Arg("title") title: string, @Arg("author") author: string): Book {
-    const book = new Book(this.books.length + 1, title, author);
-    this.books.push(book);
-    return book;
-  }
+	@Mutation(() => Book)
+	createBook(@Arg("title") title: string, @Arg("author") author: string): Book {
+		const book = new Book(this.books.length + 1, title, author);
+		this.books.push(book);
+		return book;
+	}
 
-  // idが必要とする。
-  // idに合致する本がない場合はnullを返す。
-  @Mutation(() => Book, { nullable: true })
-  updateBook(
-    @Arg("id", () => Int) id: number,
-    @Arg("title", () => String, { nullable: true }) title?: string,
-    @Arg("author", () => String, { nullable: true }) author?: string
-  ): Book | null {
-    const book = this.books.find(book => book.id === id);
-    if (!book) return null;
-    if (title) book.title = title;
-    if (author) book.author = author;
-    return book;
-  }
+	// idが必要とする。
+	// idに合致する本がない場合はnullを返す。
+	@Mutation(() => Book, { nullable: true })
+	updateBook(
+		@Arg("id", () => Int) id: number,
+		@Arg("title", () => String, { nullable: true }) title?: string,
+		@Arg("author", () => String, { nullable: true }) author?: string,
+	): Book | null {
+		const book = this.books.find((book) => book.id === id);
+		if (!book) return null;
+		if (title) book.title = title;
+		if (author) book.author = author;
+		return book;
+	}
 
-  @Mutation(() => Boolean)
-  deleteBook(@Arg("id", () => Int) id: number): boolean {
-    const bookIndex = this.books.findIndex(book => book.id === id);
-    if (bookIndex === -1) return false;
-    this.books.splice(bookIndex, 1);
-    return true;
-  }
+	@Mutation(() => Boolean)
+	deleteBook(@Arg("id", () => Int) id: number): boolean {
+		const bookIndex = this.books.findIndex((book) => book.id === id);
+		if (bookIndex === -1) return false;
+		this.books.splice(bookIndex, 1);
+		return true;
+	}
 }
 
 async function main() {
-  const schema = await buildSchema({
-    resolvers: [BookResolver],
-  });
+	const schema = await buildSchema({
+		resolvers: [BookResolver],
+	});
 
-  const server = new ApolloServer({ schema });
-  const app = express();
-  await server.start();
-  // どうしても型が合わないので、型を無理やり合わせる。
-  server.applyMiddleware({ app: app as any });
+	const server = new ApolloServer({ schema });
+	const app = express();
+	await server.start();
+	server.applyMiddleware({ app } as ServerRegistration);
 
-  app.listen(4000, () => {
-    console.log("Server is running on http://localhost:4000/graphql");
-  });
+	app.listen(4000, () => {
+		console.log("Server is running on http://localhost:4000/graphql");
+	});
 }
 
 main();
